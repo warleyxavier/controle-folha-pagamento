4058c207788ffb0d36b178d04cdaddc51f539357
warleyxavier <warley.xavier@enviou.com.br>
2021-05-19 22:52:53 -0300

    feat: Configurada conexao com banco de dados
    Configurada conexão com banco de dados. Também foi criado migrate da tabela de funcionarios.

---
+++ ControleFolhaPagamento.Aplicacao/Infraestrutura/Contexts/GlobalContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ControleFolhaPagamento.Aplicacao.Dominio.Entidades;
+
+namespace ControleFolhaPagamento.Aplicacao.Infraestrutura.Contexts
+{
+    public class GlobalContext : DbContext
+    {
+        public GlobalContext()
+        {
+        }
+
+        public GlobalContext(DbContextOptions<GlobalContext> options): base(options)
+        {
+        }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+    }
+}
