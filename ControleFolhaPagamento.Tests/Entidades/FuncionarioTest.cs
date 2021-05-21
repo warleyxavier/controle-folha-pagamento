7a300bf57c31d7864ca7059678268a03f0cb0a1c
warleyxavier <warley.xavier@enviou.com.br>
2021-05-21 15:39:00 -0300

    test: implementados testes automatizados
    Implementados testes automatizados para validar as implementações realizadas na camada da aplicação. Foram implementados testes para validar os comportamentos das seguintes classes:
    
    * Funcionario
    * ValidadorFuncionario
    * GerenciadorFuncionario
    
    Para mockar os comportamentos foi utilizada a biblioteca moq, na versão 4.

---
+++ ControleFolhaPagamento.Tests/Entidades/FuncionarioTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using ControleFolhaPagamento.Aplicacao.Dominio.Entidades;
+
+namespace ControleFolhaPagamento.Tests.Entidades
+{
+    public class FuncionarioTest
+    {
+        private readonly Funcionario funcionario;
+
+        public FuncionarioTest()
+        {
+            this.funcionario = new Funcionario();
+        }
+
+        [Theory]
+        [InlineData("12345678", "12345678")]
+        [InlineData("539.121.650-84", "53912165084")]
+        [InlineData("539.121.65084", "53912165084")]
+        [InlineData("539121650-84", "53912165084")]
+        [InlineData("53912165084", "53912165084")]
+        [InlineData("", "")]
+        public void DeveriaRemoverCaracteresDoDocumentoDoFuncionario(string documento, string documentoEsperado)
+        {
+            funcionario.Documento = documento;
+            funcionario.RemoverCaracteresDoDocumento();
+            Assert.Equal(documentoEsperado, funcionario.Documento);
+        }
+
+    }
+}
---
+++ ControleFolhaPagamento.Tests/Validadores/ValidadorFuncionarioTest.cs
@@ -0,0 +1,86 @@
+﻿using Xunit;
+using ControleFolhaPagamento.Aplicacao.Dominio.Entidades;
+using ControleFolhaPagamento.Aplicacao.Dominio.Excecoes;
+using ControleFolhaPagamento.Aplicacao.Dominio.Validadores;
+using ControleFolhaPagamento.Aplicacao.Dominio.Validadores.impl;
+using System.Linq;
+
+namespace ControleFolhaPagamento.Tests.Validadores
+{
+    public class ValidadorFuncionarioTest
+    {
+        private readonly IValidadorFuncionario validador;
+
+        public ValidadorFuncionarioTest()
+        {
+            this.validador = new ValidadorFuncionario();
+        }
+
+        [Fact]
+        public void DeveriaLancarExcecaoCasoFuncionarioEstejaInvalido()
+        {
+            var funcionario = new Funcionario();
+            var falhasEsperadas = new string[]
+            {
+                "Nome não foi informado",
+                "Sobre Nome não foi informado",
+                "Setor não foi informado",
+                "Documento não foi informado",
+                "Salario Bruto não foi informado",
+            };
+
+            ValidarLancamentoExcecaoAoValidarFuncionario(funcionario, falhasEsperadas);
+        }
+
+        [Theory]
+        [InlineData("123")]
+        [InlineData("123456")]
+        [InlineData("123456789")]
+        [InlineData("1234567890")]
+        [InlineData("123456789011")]
+        [InlineData("1.2.3.4.5.6.7.8.9.0.1")]
+        public void DeveriaLancarExcecaoCasoDocumentoFuncionarioEstejaInvalido(string documento)
+        {
+            var funcionario = new Funcionario() { Documento = documento };
+            var falhasEsperadas = new string[]
+            {
+                "O Documento informado é inválido",
+            };
+
+            ValidarLancamentoExcecaoAoValidarFuncionario(funcionario, falhasEsperadas);
+        }
+
+        private void ValidarLancamentoExcecaoAoValidarFuncionario(Funcionario funcionario, string[] falhasEsperadas)
+        {
+            var exception = Assert.Throws<ValidacaoException>(() =>
+            {
+                validador.Validar(funcionario);
+            });
+
+            foreach (var falha in falhasEsperadas)
+            {
+                Assert.True(exception.Erros.Contains(falha));
+            }
+        }
+
+        [Fact]
+        public void NaoDeveriaLancarExcecaoSeFuncionarioEstiverCorreto()
+        {
+            var funcionario = ConstruirFuncionarioComDadosValidos();
+            this.validador.Validar(funcionario);
+            Assert.True(true);
+        }
+
+        private Funcionario ConstruirFuncionarioComDadosValidos()
+        {
+            return new Funcionario()
+            {
+                Nome = "Warley",
+                SobreNome = "Andrade Xavier",
+                Documento = "94358937080",
+                Setor = "TI",
+                SalarioBruto = 50000.01
+            };
+        }
+    }
+}
