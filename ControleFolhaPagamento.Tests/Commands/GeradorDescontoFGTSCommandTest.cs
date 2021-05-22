8e5d74dcea8b7fc412daeedcb93df4604a0a4e1d
warleyxavier <warley.xavier@enviou.com.br>
2021-05-21 21:56:39 -0300

    test: implementação de testes automatizados para geradores de desconto
    Implementada primeira parte dos testes automatizados para os geradores de desconto

---
+++ ControleFolhaPagamento.Tests/Commands/GeradorDescontoFGTSCommandTest.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using ControleFolhaPagamento.Aplicacao.Dominio.Commands;
+using ControleFolhaPagamento.Aplicacao.Dominio.Commands.impl;
+using ControleFolhaPagamento.Aplicacao.Dominio.Model;
+using ControleFolhaPagamento.Aplicacao.Dominio.Enums;
+
+namespace ControleFolhaPagamento.Tests.Commands
+{
+    public class GeradorDescontoFGTSCommandTest
+    {
+        private readonly IGeradorDescontoCommand geradorDesconto;
+
+        public GeradorDescontoFGTSCommandTest()
+        {
+            this.geradorDesconto = new GeradorDescontoFGTSCommand();
+        }
+
+        [Fact]
+        public void SempreDeveriaPoderGerarDesconto()
+        {
+            Assert.True(geradorDesconto.DeveGerar(new Funcionario()));
+        }
+
+        [Theory]
+        [InlineData(100,8)]
+        [InlineData(1000,80)]
+        [InlineData(150.80,12.064)]
+        public void DeveriaGerarDesconto(double salario, double descontoEsperado)
+        {
+            var funcionario = new Funcionario()
+            {
+                SalarioBruto = salario
+            };
+
+            Lancamento desconto = this.geradorDesconto.Gerar(funcionario);
+
+            Assert.Equal(TipoLancamento.Desconto, desconto.Tipo);
+            Assert.Equal("FGTS", desconto.Descricao);
+            Assert.Equal(descontoEsperado, desconto.Valor);
+        }
+    }
+}
---
+++ ControleFolhaPagamento.Tests/Commands/GeradorDescontoPlanoDentalCommandTest.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using ControleFolhaPagamento.Aplicacao.Dominio.Commands;
+using ControleFolhaPagamento.Aplicacao.Dominio.Commands.impl;
+using ControleFolhaPagamento.Aplicacao.Dominio.Model;
+using ControleFolhaPagamento.Aplicacao.Dominio.Enums;
+
+namespace ControleFolhaPagamento.Tests.Commands
+{
+    public class GeradorDescontoPlanoDentalCommandTest
+    {
+        private readonly IGeradorDescontoCommand geradorDesconto;
+
+        public GeradorDescontoPlanoDentalCommandTest()
+        {
+            this.geradorDesconto = new GeradorDescontoPlanoDentalCommand();
+        }
+
+        [Theory]
+        [InlineData(true,true)]
+        [InlineData(false, false)]
+        public void DeveriaRetornarSePodeGerarComBaseNaConfiguracaoDoFuncionario(bool funcionarioPossuiPlanoDental, bool deveriaPoderGerarDesconto)
+        {
+            var funcionario = new Funcionario()
+            {
+                PossuiPlanoDental = funcionarioPossuiPlanoDental
+            };
+
+            bool podeGerarDesconto = this.geradorDesconto.DeveGerar(funcionario);
+
+            Assert.Equal(deveriaPoderGerarDesconto, podeGerarDesconto);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(150.80)]
+        public void DeveriaGerarDesconto(float salario)
+        {
+            const double VALOR_DE_DESCONTO_ESPERADO = 5.00;
+
+            var funcionario = new Funcionario()
+            {
+                SalarioBruto = salario
+            };
+
+            Lancamento desconto = this.geradorDesconto.Gerar(funcionario);
+
+            Assert.Equal(TipoLancamento.Desconto, desconto.Tipo);
+            Assert.Equal("Plano Dental", desconto.Descricao);
+            Assert.Equal(VALOR_DE_DESCONTO_ESPERADO, desconto.Valor);
+        }
+
+    }
+}
